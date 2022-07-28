c737e16296e65381f0370707c50e877cee6f83c2
wxc10dev <97353084+wxc10dev@users.noreply.github.com>
2022-07-28 23:28:00 +0200

    New Inputs added. Style Inproved.

--- src/index.js
+++ src/index.js
@@ -5,10 +5,20 @@ const app = document.getElementById("app");
 const userInput_1 = document.createElement("input");
 const userInput_2 = document.createElement("input");
 const userInput_3 = document.createElement("input");
+const userInput_4 = document.createElement("input");
+const userInput_5 = document.createElement("input");
+const userInput_6 = document.createElement("input");
+const userInput_7 = document.createElement("input");
+const userInput_8 = document.createElement("input");
 
 app.append(userInput_1);
 app.append(userInput_2);
 app.append(userInput_3);
+app.append(userInput_4);
+app.append(userInput_5);
+app.append(userInput_6);
+app.append(userInput_7);
+app.append(userInput_8);
 
 // User input 1
 userInput_1.setAttribute("type", "text");
@@ -22,11 +32,41 @@ userInput_2.setAttribute("class", "user-input");
 userInput_2.setAttribute("id", "input-2");
 userInput_2.setAttribute("value", "Acheter de la salade verte");
 
-// User input 2
+// User input 3
 userInput_3.setAttribute("type", "text");
 userInput_3.setAttribute("class", "user-input");
 userInput_3.setAttribute("id", "input-3");
 userInput_3.setAttribute("value", "Acheter de la salade verte");
+
+// User input 4
+userInput_4.setAttribute("type", "text");
+userInput_4.setAttribute("class", "user-input");
+userInput_4.setAttribute("id", "input-4");
+userInput_4.setAttribute("value", "Acheter de la salade verte");
+
+// User input 5
+userInput_5.setAttribute("type", "text");
+userInput_5.setAttribute("class", "user-input");
+userInput_5.setAttribute("id", "input-5");
+userInput_5.setAttribute("value", "Acheter de la salade verte");
+
+// User input 6
+userInput_6.setAttribute("type", "text");
+userInput_6.setAttribute("class", "user-input");
+userInput_6.setAttribute("id", "input-6");
+userInput_6.setAttribute("value", "Acheter de la salade verte");
+
+// User input 7
+userInput_7.setAttribute("type", "text");
+userInput_7.setAttribute("class", "user-input");
+userInput_7.setAttribute("id", "input-7");
+userInput_7.setAttribute("value", "Acheter de la salade verte");
+
+// User input 8
+userInput_8.setAttribute("type", "text");
+userInput_8.setAttribute("class", "user-input");
+userInput_8.setAttribute("id", "input-8");
+userInput_8.setAttribute("value", "Acheter de la salade verte");
 
 // Log
 console.log(app);
